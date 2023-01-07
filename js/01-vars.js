18ed9e5aadb443492c85035fd43b2bade2e7f38f
Настя Скрицька <snastia.m@gmail.com>
2023-01-07 12:51:36 +0200

    sdfyvgbnjkm

---
+++ js/01-vars.js
@@ -0,0 +1,68 @@
+"Java script".length 
+
+// це (length) визначає скільки елементів в одному рядку (букв, пропусків)
+
+let myNamE = "Poly";
+
+let numbeR = 10; 
+
+// використовуємо let тільки тоді, коли знаємо що будемо перевикористовувати
+// одинакових імен не може бути. кожне повинне бути унікальним
+// не можна писати console.log(number), її значення ДО let number = 10.
+// якщо треба написати перед name ще одне слово, name пишемо з великої букви. приклад: myName
+// і так далі. просто так не пишемо. потрібно писати з let. приклад вище
+
+console.log(1);
+console.log(numbeR);
+console.log("Hey");
+
+// так можна писати, коли я хочу побачити щось в консолі 
+
+ const number = 15;
+console.log(number);
+
+// так потрібно робити, коли я хочу щоб далі по коду число змінилось, з 10 на 15 наприклад
+
+// ПРИМІТИВНІ 
+
+// number
+const age = 25;
+console.log(age);
+
+// string
+const userName = "Chelsy";
+console.log(userName);
+
+// booLean
+
+const isOnline = true;
+console.log(isOnline);
+
+// undefined
+
+console.log(undefined);
+
+// null
+
+console.log(null);
+
+// noteandnumber
+
+console.log(NaN);
+
+
+// спочатку краще створювати через const. якщо далі потрібно буде змінити age, повертаємося і пишемо let
+//  маленька практика
+
+const myName = "nastya";
+console.log(myName);
+
+const numbere = 6;
+console.log(numbere);
+
+const isActive = false;
+console.log(isActive);
+
+// стрінг, числове значення, - true. інше - false (число 0, "", пустий рядок також).
+
+
---
+++ js/02-vars.js
@@ -0,0 +1,28 @@
+// const message = "ви впевнені?"
+// alert(message);
+// або просто window.alert(). це також правильно. це те, що викликає смс на сайті
+
+// const userAnswer = confirm(message);
+// console.log(userAnswer);
+
+// prompt("що ви хочете");
+
+// з можливістю відповісти
+
+// const answer = prompt ("чи ви хочете продовжити підписку?");
+// console.log(answer);
+
+console.log(5 % 2);
+// залишок є, якщо ділиться нерівно
+
+// let sum = 10 / 2;
+// const result = 5 > 10;
+// console.log(result);
+// sum = sum + 2;
+// або можна sum += 5;
+const a = 5;
+const b = 10;
+const c = "5";
+const d = "Hello";
+
+console.log(a !== b);
