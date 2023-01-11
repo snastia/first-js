5a9e428933ca8c6830e9e0d1bb88063574880df2
Настя Скрицька <snastia.m@gmail.com>
2023-01-11 20:21:25 +0200

    etr

--- js/01-hw.js
+++ js/01-hw.js
@@ -38,8 +38,35 @@ let b = 10;
 console.log(a * b);
 
 // 6
-a1 = 5 + 3;
-a2 = 5 - 3;
-a3 = 5 * 3;
-a4 = 5/3;
-let a1 = 5 + 3;
+
+const a1 = 5 + 3;
+const a2 = 5 - 3;
+const a3 = 5 * 3;
+const a4 = 5/3;
+
+console.log(a1);
+console.log(a2);
+console.log(a3);
+console.log(a4);
+
+// 7
+
+const a6 = 5 % 3;
+const a7 = 3 % 5;
+const a8 = 5 + "3";
+const a9 = "5" - 3;
+const a10 = 75 + "кг";
+
+alert(a6);
+alert(a7);
+alert(a8);
+alert(a9);
+alert(a10);
+
+// 8
+
+const S = 0;
+const height = "23см";
+const width = "10см";
+S = height * width;
+console.log(S);
