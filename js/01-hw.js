4b435da96913fb34edfb3baeed535cd5bb618d54
Настя Скрицька <snastia.m@gmail.com>
2023-01-11 15:25:40 +0200

    first-hw

---
+++ js/01-hw.js
@@ -0,0 +1,45 @@
+// 1
+const str = "Привіт";
+const num = 123;
+const flag = true;
+const txt = "true";
+
+console.log(typeof str);
+console.log(typeof num);
+console.log(typeof flag);
+console.log(typeof txt);
+
+// 2
+let age = 15;
+let char = "name";
+let long = "people";
+let float = "green";
+let int = "9";
+const goto = 16;
+const name = "nastya";
+const isOnline = false;
+const number = 8;
+const volatile = "text";
+
+// 3
+const userName = prompt ("Ім'я користувача?");
+console.log(userName);
+
+// 4
+
+const myAge = 13;
+const myFriendAge = 14;
+alert(myAge * myFriendAge);
+
+// 5
+
+let a = 8;
+let b = 10;
+console.log(a * b);
+
+// 6
+a1 = 5 + 3;
+a2 = 5 - 3;
+a3 = 5 * 3;
+a4 = 5/3;
+let a1 = 5 + 3;
