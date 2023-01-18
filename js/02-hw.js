e6a656b5449d7bd9a921bc9b83d5245e7775d989
Настя Скрицька <snastia.m@gmail.com>
2023-01-18 13:32:35 +0200

    regerggge

---
+++ js/02-hw.js
@@ -0,0 +1,31 @@
+// 1
+
+let k1 = "5px";
+Number.parseInt("5px");
+
+let k2 = "12djd334";
+Number.parseInt("12djd334");
+
+let k3 = "12.45asdwe";
+Number.parseInt("12.45asdwe");
+
+let k4 = "qwqweeewq";
+Number.parseInt("qwqweeewq");
+
+// 2
+
+k1 = "5px";
+Number.parseFloat("5px");
+
+k2 = "12djd334";
+Number.parseFloat("12djd334");
+
+k3 = "12.45asdwe";
+Number.parseFloat("12.45asdwe");
+
+k4 = "qwqweeewq";
+Number.parseFloat("qwqweeewq");
+
+// 3
+
+
