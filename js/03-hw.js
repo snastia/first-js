aab10c6100d2a0e12d4ec590d46ffbe157b3c8cf
Настя Скрицька <snastia.m@gmail.com>
2023-01-25 14:15:54 +0200

    апкппвпа

---
+++ js/03-hw.js
@@ -0,0 +1,17 @@
+// 1
+
+const max = 99;
+const min = 55;
+const number  = prompt("чи попадає ваше число у діапазон з 55 до 99?")
+let message;
+if(min <= 54){
+  message = prompt("Число потрапляє у діапазон");
+} else{
+  message = prompt("Число не потрапляє у діапазон");
+}
+
+// 2
+
+const first = 0-16;
+const second = 17-60;
+const third = 61-100;
