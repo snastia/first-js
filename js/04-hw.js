6c1288a16c742f6f6149e41c82f54311b3d8e733
Настя Скрицька <snastia.m@gmail.com>
2023-01-30 20:03:01 +0200

    34567

--- js/04-hw.js
+++ js/04-hw.js
@@ -1,13 +1,22 @@
 // 1
 
-for(let i = 1; i <= 5; i += 1){
-    console.log(i);
-}
+// for(let i = 1; i <= 5; i += 1){
+//     console.log(i);
+// }
 
 // 2
 
-const min = 1;
-const max = 10;
-let total = 1;
+// const min = 1;
+// const max = 10;
+// let total = 1;
+
+// 3
 
 
+// 4
+
+// let heroName = "";
+// while(heroName.length <= 5){
+//     console.log(heroName);
+//     heroName = prompt("Введіть ім'я героя");
+// }
