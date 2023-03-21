f1a6df6d8a84eeb58d36603a26ee7f21b7a7062f
Настя Скрицька <snastia.m@gmail.com>
2023-03-21 14:45:34 +0200

    fefef

--- js/11/11-hw.js
+++ js/11/11-hw.js
@@ -6,3 +6,22 @@ console.log(titleEl)
 
 // 2
 
+const textEl = document.querySelector(".paragraph")
+textEl.textContent = "риба"
+console.log(textEl)
+
+// 3
+
+const listEl = document.querySelectorAll(".list-item")
+console.log(listEl)
+
+// 4
+
+const imageEl = document.querySelector(".image")
+imageEl.src = "https://images.pexels.com/photos/7521210/pexels-photo-7521210.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+
+// 5
+
+// const sectionTitleElleEl) = document.querySelector(".section-title"[-1])
+// sectionTitleEl.textContent = "Новий заголовок"
+// console.log(sectionTitleEl)
